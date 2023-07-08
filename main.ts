13b74ede8ae18962fb2ef41586b73d9071ce7686
Tweak <ynan243@gmail.com>
2023-07-08 04:22:32 -0600

    Add toggle for eddy beacon, change wait, add indicator.

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
+let btEnabled = false;
 
 basic.forever(function () {
-    control.waitMicros(5000)
+    control.waitMicros(10000) // every 10 seconds
+    btEnabled ? broadcastTemp() : led.unplot(0, 0); // if broadcasting temp via eddy beacon is enabled.
+    input.onButtonPressed(Button.A, () => {
+        basic.showNumber(input.temperature());
+    });
+    input.onButtonPressed(Button.B, () => {
+        btEnabled = !btEnabled;
+    });
+})
+
+function broadcastTemp() {
+    led.plot(0,0) // indicate that URL is currently being broadcasted
     bluetooth.advertiseUrl(
         `http://${input.temperature()}`,
         7,
         false
     );
-    input.onButtonPressed(Button.A, () => {
-        basic.showNumber(input.temperature());
-    })
-})
+};
