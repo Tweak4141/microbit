a76d71da2d3d6bbde9cb680cd2fdea198439dff7
Tweak <ynan243@gmail.com>
2023-07-08 04:45:30 -0600

    V2

--- main.ts
+++ main.ts
@@ -1,21 +1,31 @@
 let btEnabled = false;
-
+let persistentDisplay = false;
+let temp = updateTemp()
 basic.forever(function () {
-    control.waitMicros(10000) // every 10 seconds
+    updateTemp();
     btEnabled ? broadcastTemp() : led.unplot(0, 0); // if broadcasting temp via eddy beacon is enabled.
+    if (persistentDisplay) basic.showNumber(temp);
     input.onButtonPressed(Button.A, () => {
-        basic.showNumber(input.temperature() - 1);
+        basic.showNumber(temp);
     });
     input.onButtonPressed(Button.B, () => {
         btEnabled = !btEnabled;
     });
+    input.onButtonPressed(Button.AB, () => {
+        persistentDisplay = !persistentDisplay;
+    });
 })
 
+
 function broadcastTemp() {
-    led.plot(0,0) // indicate that URL is currently being broadcasted
+    led.plot(0,0); // indicate that URL is currently being broadcasted
     bluetooth.advertiseUrl(
-        `http://${input.temperature() - 1}`,
+        `http://${temp}`,
         7,
         false
     );
 };
+
+function updateTemp() {
+    return input.temperature() - 1;
+}
